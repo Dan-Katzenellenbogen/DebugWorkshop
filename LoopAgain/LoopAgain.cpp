1071823cbb0c41057c5b959cec8e585a94a5efb4
Dan-Katzenellenbogen <150925149+Dan-Katzenellenbogen@users.noreply.github.com>
2025-12-03 18:40:18 +0200

    fix: fixed the loop again

--- LoopAgain/LoopAgain.cpp
+++ LoopAgain/LoopAgain.cpp
@@ -2,16 +2,20 @@
 
 int main()
 {
-	unsigned int size = 0;
+	int size = 0;
 
 	int t1 = 0, t2 = 1;
 
 	std::cout << "what is the size of the series? ";
 	std::cin >> size;
 
-	while (size >= 0)
+	while (size > 0)
 	{
-		std::cout << t1 << ", ";
+		std::cout << t1;
+		if (size != 1)
+		{
+			std::cout << ", ";
+		}
 
 		int nextTerm = t1 + t2;
 		t1 = t2;
